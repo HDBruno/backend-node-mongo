966c9b172d277fd59e03e3ec6ba7c2c889724a2a
Hernan D. Bruno <hernan.bruno@outlook.com>
2023-01-19 20:23:48 -0300

    feat: main app updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+const initDB = require("./database/mongo.js");
 const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,10 +14,14 @@ app.use(cors());
 
 //importo el router y se lo asigno a una ruta.
 const v1RouterIndex = require('./v1/routes/index');
-const v1RouterLogin = require('./v1/routes/login');
+const v1RouterLogin = require('./v1/routes/auth');
+const v1RouterUsers = require('./v1/routes/users');
 app.use('/api/v1', v1RouterIndex);
 app.use('/api/v1', v1RouterLogin);
+app.use('/api/v1', v1RouterUsers);
 
 app.listen(PORT, () => {
     console.log(`Server listen in port ${PORT}...`)
 });
+
+initDB();
