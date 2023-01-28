439d26ac90ae43869b8e76061ea4bc8f966db95b
Hernan D. Bruno <hernan.bruno@outlook.com>
2023-01-28 05:46:35 -0300

    feat: refactor mongo config

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,25 +1,24 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
+  const connect = () => {
+    const DB_URI = process.env.DB_URI;
+    mongoose.connect(
+      DB_URI,
+      {
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      (err) => {
+        if (err) {
+          console.log("MongoDB connection error.", err);
+        } else {
+          console.log("MongoDB connection success.");
+        }
+      }
+    );
+  };
 
-    const connect = () => {
-        mongoose.connect(
-            process.env.DB_URI,
-            {
-                keepAlive: true,
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            },
-            (err) => {
-                if (err) {
-                    console.log("MongoDB connection error.");
-                } else {
-                    console.log("MongoDB connection success.");
-                }
-            }
-        )
-    }
-
-    connect();
-}   
-
+  connect();
+};
